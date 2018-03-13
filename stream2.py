e5c5edac7abd411f81558f74e8229f674e72218e
Steven Lagrange <slagrang@ualberta.ca>
2018-03-13 22:38:08 +0000

    Creating stream client

--- stream2.py
+++ stream2.py
@@ -1,49 +1,44 @@
-import io
+from io import BytesIO
 import socket
 import struct
 import time
-import picamera
+from picamera import PiCamera
 import modules.streaming as Streaming
 
 # Connect a client socket to my_server:8000 (change my_server to the
 # hostname of your server)
-url = 'http://crawler-stream.us-west-2.elasticbeanstalk.com/'
-client_socket = socket.socket()
-client_socket.connect(('url', 8000))
+aws_url = 'http://crawler-stream.us-west-2.elasticbeanstalk.com/'
+home_url = '192.168.0.3'
+port = 8000
 
-# Make a file-like object out of the connection
+client_socket = socket.socket()
+client_socket.connect((home_url, 8000))
 connection = client_socket.makefile('wb')
 
-
 try:
-    camera = picamera.PiCamera()
-    camera.resolution = (640, 480)
-    # Start a preview and let the camera warm up for 2 seconds
-    camera.start_preview()
-    time.sleep(2)
-
-    # Note the start time and construct a stream to hold image data
-    # temporarily (we could write it directly to connection but in this
-    # case we want to find out the size of each capture first to keep
-    # our protocol simple)
     start = time.time()
-    stream = io.BytesIO()
-    for foo in camera.capture_continuous(stream, 'jpeg'):
-        # Write the length of the capture to the stream and flush to
-        # ensure it actually gets sent
-        connection.write(struct.pack('<L', stream.tell()))
-        connection.flush()
-        # Rewind the stream and send the image data over the wire
-        stream.seek(0)
-        connection.write(stream.read())
-        # If we've been capturing for more than 30 seconds, quit
-        if time.time() - start > 30:
-            break
-        # Reset the stream for the next capture
-        stream.seek(0)
-        stream.truncate()
-    # Write a length of zero to the stream to signal we're done
-    connection.write(struct.pack('<L', 0))
+    
+    with PiCamera(resolution='640x480', framerate=24) as camera:
+        stream = BytesIO()
+        camera.start_recording(stream, format='mjpeg')
+    
+        while True:
+            print(stream.tell())
+            connection.write(struct.pack('<L', stream.tell()))
+            connection.flush()
+            stream.seek(0)
+            connection.write(stream.read())
+            
+            if time.time() - start > 30:
+                break
+            
+            stream.seek(0)
+            stream.truncate()
+        
+        connection.write(struct.pack('<L', 0))
+        
+
 finally:
     connection.close()
     client_socket.close()
+
